d1c7b5a3165e32932d606cf193eaea4270a1819b
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-01-28 19:24:02 +0000

    Kattaroq qil (_for element img_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,8 +7,8 @@ const Header = () => {
         <Image
           src="https://i.postimg.cc/SxrGfD79/Screenshot-2026-01-29-at-00-10-21.png"
           alt="Shariah Standards Pro Course"
-          width={82}
-          height={100}
+          width={123}
+          height={150}
           className="object-contain"
         />
       </div>
