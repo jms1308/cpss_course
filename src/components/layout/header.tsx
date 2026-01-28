39f0b68a5241fd51470de6cccfa3a32da63ee753
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-01-28 19:23:34 +0000

    tepa qismga shu photoni qo'y. O'rtaga https://i.postimg.cc/SxrGfD79/Scre

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image';
+
 const Header = () => {
   return (
     <header className="bg-card w-full py-6">
       <div className="container mx-auto flex justify-center items-center">
-        
+        <Image
+          src="https://i.postimg.cc/SxrGfD79/Screenshot-2026-01-29-at-00-10-21.png"
+          alt="Shariah Standards Pro Course"
+          width={82}
+          height={100}
+          className="object-contain"
+        />
       </div>
     </header>
   );
