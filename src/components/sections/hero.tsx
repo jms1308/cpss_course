1c3671923fc3ce8701567a49d15ae5ee5e0cab0e
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-01-28 19:56:38 +0000

    top margin va paddingni kichraytir, space qolmasin  (_for element sectio

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -4,9 +4,9 @@ import { Button } from '@/components/ui/button';
 
 const Hero = () => {
   return (
-    <section className="w-full bg-card overflow-hidden pt-8">
+    <section className="w-full bg-card overflow-hidden">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid md:grid-cols-2 gap-8 items-center pt-12 pb-20 md:py-28">
+        <div className="grid md:grid-cols-2 gap-8 items-center pb-20 md:pb-28">
           <div className="space-y-8 animate-in fade-in slide-in-from-left-12 duration-1000 text-center md:text-left">
             <div className="font-headline font-bold leading-none tracking-tight">
                 <span className="text-7xl md:text-8xl text-accent">CPSS</span>
