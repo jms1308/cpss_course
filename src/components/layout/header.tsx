16f8835fa03cdeb9de226fe645840ceadb1d786a
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-01-28 19:24:57 +0000

    Shuni to'ldirib tur (_for element header_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,16 +2,13 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className="bg-card w-full py-6">
-      <div className="container mx-auto flex justify-center items-center">
-        <Image
-          src="https://i.postimg.cc/SxrGfD79/Screenshot-2026-01-29-at-00-10-21.png"
-          alt="Shariah Standards Pro Course"
-          width={123}
-          height={150}
-          className="object-contain"
-        />
-      </div>
+    <header className="bg-card w-full relative h-56">
+      <Image
+        src="https://i.postimg.cc/SxrGfD79/Screenshot-2026-01-29-at-00-10-21.png"
+        alt="Shariah Standards Pro Course"
+        fill
+        className="object-contain"
+      />
     </header>
   );
 };
