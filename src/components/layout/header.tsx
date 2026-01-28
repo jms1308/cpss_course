d11a5ec6d60856d3fed98fd8fb5113a770dc17c3
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-01-28 19:22:11 +0000

    Main oagedai birinchi rasmni delkete qil, O'ngdagini emas

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,16 +1,8 @@
-import Image from 'next/image';
-
 const Header = () => {
   return (
     <header className="bg-card w-full py-6">
       <div className="container mx-auto flex justify-center items-center">
-        <Image
-          src="https://i.postimg.cc/FHRYwpX6/Screenshot-2026-01-29-at-00-12-59.png"
-          alt="Partner logos"
-          width={600}
-          height={40}
-          className="object-contain px-4"
-        />
+        
       </div>
     </header>
   );
