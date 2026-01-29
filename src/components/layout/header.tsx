3f4db7a1c55f5929715c7badae94099bd4ad6645
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-01-29 12:05:35 +0000

    Menuni height ni kengyitr (_for element img_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className="bg-card w-full relative h-14 pt-6">
+    <header className="bg-card w-full relative h-20">
       <Image
         src="https://i.postimg.cc/CxcJGcS2/Screenshot-2026-01-29-at-00-53-01.png"
         alt="Shariah Standards Pro Course"
