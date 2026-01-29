2ddae0263317de5cdc72ffbd2641597a2eb4d102
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-01-29 12:04:49 +0000

    Menu top qismiga ko'rpq space qo'sh 10px

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className="bg-card w-full relative h-14 pt-4">
+    <header className="bg-card w-full relative h-14 pt-6">
       <Image
         src="https://i.postimg.cc/CxcJGcS2/Screenshot-2026-01-29-at-00-53-01.png"
         alt="Shariah Standards Pro Course"
