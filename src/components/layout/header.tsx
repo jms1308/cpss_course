fdefb75ea06b8eb9c722d32e53e2e2db1f1a31ea
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2026-02-02 07:36:13 +0000

    Menudagi rasmni https://i.postimg.cc/y6ytvbRg/Screenshot-2026-02-02-at-1

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-card w-full relative h-20">
       <Image
-        src="https://i.postimg.cc/CxcJGcS2/Screenshot-2026-01-29-at-00-53-01.png"
+        src="https://i.postimg.cc/y6ytvbRg/Screenshot-2026-02-02-at-12-29-40.png"
         alt="Shariah Standards Pro Course"
         fill
         className="object-contain"
